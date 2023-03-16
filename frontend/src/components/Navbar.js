a426322e475b9b7d239f9a1a02b5667ae82e91ea
Kainulainen Oskari Aleksanteri <oskari.a.kainulainen@student.jyu.fi>
2023-03-16 20:34:45 +0200

    Homepage modded

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,21 @@
 import { Link } from 'react-router-dom';
 import Logo from "../images/Logo.png"
+import Faceicon from "../images/Faceicon.png"
 import './Navbar.css'
 
 const Navbar = () => {
     return (
-      <nav className="Navbar">
-        <img src={Logo} className='navLogo'></img>
+      <div className="Navbar">
+      <img src={Logo} className='navLogo'/>
+      <nav className="Links">
         <Link to="/" className='navLink'>Home</Link>
         <Link to="/about" className='navLink'>About</Link>
         <Link to="/gigs" className='navLink'>Gigs</Link>
+        <Link to="https://www.facebook.com/TrioHankkijat">
+          <img src={Faceicon} className='faceLink'></img>
+        </Link>
       </nav>
+      </div>
     );
   };
   
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,9 @@
+import { Link } from 'react-router-dom';
+
 import Bandimage5 from "../images/Bandimage3.jpg"
+import AboutButton from "../images/AboutButton.jpg";
+import GigsButton from "../images/GigsButton.jpg";
+
 import './Home.css'
 
 const Home = () => {
@@ -6,6 +11,18 @@ const Home = () => {
       <div className="homeContainer">
         <div className="banner">
           <img src={Bandimage5} className='bandHome'></img>
+        </div>
+        <div className="homeAbout">
+          <h1 id="h1">Trio Hankkijat</h1>
+          <h2 id="h2">Yhtye joka soittaa kotimaista iskelmää sydämellä</h2>
+        </div>
+        <div className="imgButtons">
+          <Link to="/about">
+            <img src={AboutButton} className='imgButtonleft'></img>
+          </Link>
+          <Link to="/gigs">
+            <img src={GigsButton} className='imgButtonright'></img>
+          </Link>
         </div>
       </div>
     );
